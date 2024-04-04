f94cce20d58c35a8423fcda1523fd2399d5b5741
ChowYong Ng <ngchowyong@hotmail.com>
2024-04-04 20:43:14 +0800

    initialize a new project called basic c this project defines common structure of a project and its .c/.h file template

---
+++ basic_c/host_machine/inc/computation.h
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------
+ *  computation.h - provide common computation functions library
+ *
+ *  @date   2024/04/04
+ *
+ *  @author ChowYong Ng
+ *
+ *  @description:
+ *  Provide basic and common computation functions for other module's
+ *
+ *  Copyright stated here...
+ *------------------------------------------------------------------
+ */
+
+/* Prevent recursive inclusion */
+#ifndef __COMPUTATION_H__
+#define __COMPUTATION_H__
+
+/* Includes */
+
+
+/* Defines */
+
+
+/* Functions */
+void sort_arr(uint16_t * arr, uint16_t size);
+
+#endif /* __COMPUTATION_H__ */
---
+++ basic_c/host_machine/src/computation.c
@@ -0,0 +1,32 @@
+/*------------------------------------------------------------------
+ *  computation.c - provide common computation functions library
+ *
+ *  @date   2024/04/04
+ *
+ *  @author ChowYong Ng
+ *
+ *  @description:
+ *  Provide basic and common computation functions for other module's
+ *
+ *------------------------------------------------------------------
+ */
+
+
+/* Includes */
+#include "computation.h"
+
+
+/* Defines */
+
+
+/* Functions */
+
+/**
+* @description sort incoming array in ascending order
+* @param       arr - pointer to the incoming array
+* @param       size - length of array arr
+* @retval      void
+**/
+void sort_arr(uint16_t * arr, uint16_t size){
+  // sort array
+}
